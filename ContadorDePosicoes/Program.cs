6fb1d66d3065de5bada1c108c58ab0301de0df56
Rodolfo Gaspar <dolfogaspar@gmail.com>
2017-06-04 18:46:04 -0300

    processamento inicial

--- ContadorDePosicoes/Program.cs
+++ ContadorDePosicoes/Program.cs
@@ -10,6 +10,52 @@ namespace ContadorDePosicoes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting...\n");
+
+            Console.WriteLine("Digite a quantidade total de posições do arquivo:");
+            string total = Console.ReadLine();
+            Console.WriteLine(string.Format("Valor digitado: {0}", total));
+
+            Console.WriteLine("Confirma? (S/N)");
+            if (Console.ReadLine().ToString().ToUpper().Equals("S"))
+            {
+                if (Convert.ToInt32(total) >= 10)
+                {
+                    int a = 0;
+                    for (int i = 1; i != Convert.ToInt32(total); i++)
+                    {
+                        Console.Write(i);
+                        if (i == 9)
+                        {
+                            i = 0;
+                            Console.Write(i);
+                            a++;
+                        }
+
+                        if (a == Convert.ToInt32(total) / 10)
+                        {
+                            Console.WriteLine();
+                            var aa = -10;
+                            for (int ii = 0; ii != 10; ii++)
+                            {
+                                Console.Write(ii);                                
+                                aa++;
+                                if (aa == 0)
+                                {
+                                    aa = -10;
+                                    ii++;
+                                }
+                                else
+                                { ii--; }
+                            }
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
